871caba5f20f34d107f603698f6daf3361c04657
archaudhari <archaudhari98@gmail.com>
2022-07-17 20:44:34 +0530

    React_Portfolio

---
+++ React-Portfolio/src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Ajay Chaudhari",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/ajay-chaudhari-77a172205/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "As someone who understands web application architecture well, I am capable of training others in general web development and can lead as well be a good team employee.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/ajay-chaudhari-77a172205/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/archaudhari",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/archaudhari98",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently learning product engineering at School of Accelerated Learning and pursuing my course from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"School of Accelerated Learning",
+        "specialization":"Product Engineering [MERN-Stack Development]",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2022"
+      },
+      {
+        "UniversityName":"SunBeam",
+        "specialization":"Computer Fundamentals [C Prog., DSA, OOP's using C++]",
+        "MonthOfPassing":"March 2021 - May 2021",
+        "YearOfPassing":""
+      },
+      {
+        "UniversityName":"University of Pune",
+        "specialization":"Mechanical Engineering",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2020"
+      },
+      
+    ],
+    "work":[
+      {
+        "CompanyName":"Sancy Berhad",
+        "specialization":"Software Engineer",
+        "MonthOfLeaving":"Dec 2021",
+        "Achievements":"Developed and executed test procedures for software components & Planned, tracked, and managed deliverables on deployments",
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"E-Commerce",
+        "imgurl":"images/portfolio/e-commerce.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
