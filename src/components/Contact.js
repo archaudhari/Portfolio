23d8e6b23aceb384411427d89468be976e5bfe82
Ajay Chaudhari <81674825+archaudhari@users.noreply.github.com>
2022-11-15 22:32:48 +0530

    new fields are  added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -45,8 +45,8 @@ export default function Contact() {
                 ADDRESS
               </h2>
               <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
+              PICT COLLEGE BUS STOP, FV42+VHW <br />
+              Near Bharati Vidyapeeth Campus, Pune Maharashtra 411043
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -54,12 +54,12 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+                archaudhari98@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">+91 9657597880</p>
             </div>
           </div>
         </div>
